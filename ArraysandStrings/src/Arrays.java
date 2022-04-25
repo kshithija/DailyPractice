6391f08c5cb28f99b0a5a2fcf29dcce6e2e52d3d
kshithijakappa <kshithijakappa@gmail.com>
2022-04-25 11:53:43 -0400

    P1 - Added Arrays Program

---
+++ ArraysandStrings/src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int a[] = new int[4];
+        for(int i=0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args){
+        int[] a={10,20,30,40};
+        System.out.println(a.length);
+        a[2]=50;
+        System.out.println(a[2]);
+        System.out.println(a[1]);
+    }
+}
+
