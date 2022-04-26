6e3cc0bf5f03d4a5c1028c519d2a4b74a0b85f8c
kshithijakappa <kshithijakappa@gmail.com>
2022-04-26 00:53:40 -0400

    P1 - Big Integer prime

---
+++ ArraysandStrings/src/BigInteger/PrimeandNextPrime.java
@@ -0,0 +1,22 @@
+package BigInteger;
+
+import java.math.BigInteger;
+
+public class PrimeandNextPrime {
+    public static void main(String[] args) {
+        int n=17;
+        System.out.println(isPrime(n));
+        System.out.println(nextPrime(n));
+    }
+
+    private static int nextPrime(int n) {
+        BigInteger b = BigInteger.valueOf(n);
+        String s = b.nextProbablePrime().toString();
+        return Integer.parseInt(s);
+    }
+
+    private static boolean isPrime(int n) {
+        BigInteger b = BigInteger.valueOf(n);
+        return b.isProbablePrime(1);
+    }
+}
