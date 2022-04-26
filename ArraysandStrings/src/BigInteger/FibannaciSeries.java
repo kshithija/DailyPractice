9449160888a1159c20056d3c382799e089ec4b79
kshithijakappa <kshithijakappa@gmail.com>
2022-04-26 00:09:16 -0400

    P1 - Big Integer fibanacci

---
+++ ArraysandStrings/src/BigInteger/FibannaciSeries.java
@@ -0,0 +1,34 @@
+package BigInteger;
+
+import java.math.BigInteger;
+
+public class FibannaciSeries {
+    public static void main(String[] args) {
+        int n =1000; // 0 1 1 2 3 5
+      //  System.out.println(getFibannaciNumber(n));
+        System.out.println(getBifIntegerFibannaciNumber(n));
+    }
+
+    private static int getFibannaciNumber(int n) {
+        int fib1=0, fib2=1, fib3=1;
+        for(int i=2;i<=n;i++){
+            fib3=fib1+fib2;
+            fib1 = fib2;
+            fib2 = fib3;
+        }
+        return fib2;
+    }
+
+    private static BigInteger getBifIntegerFibannaciNumber(int n) {
+        BigInteger a = new BigInteger("0");
+        BigInteger b = new BigInteger("1");
+        BigInteger c = new BigInteger("1");
+        for(int i=2;i<=n;i++){
+            c=a.add(b);
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
